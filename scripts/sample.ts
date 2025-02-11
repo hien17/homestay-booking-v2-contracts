cdba02fe08321c5335ae7e46e202fe1141ab12fb
Hien <timowerner11germany@gmail.com>
2025-02-11 15:58:50 +0700

    chore: add vscode settings

--- scripts/sample.ts
+++ scripts/sample.ts
@@ -1,29 +1,30 @@
 import { Deployer } from "./helper";
-import hre, { ethers } from "hardhat";
+import hre from "hardhat";
 
 async function main() {
-    const accounts = await hre.ethers.getSigners();
-    const account_num = 0
-    const confirmnum = 2
+  const accounts = await hre.ethers.getSigners();
+  const account_num = 0;
+  const confirmnum = 2;
 
-    const account = accounts[account_num];
-    const network = hre.network.name
-    console.log(`Submit transactions with account: ${account.address} on ${network}`)
+  const account = accounts[account_num];
+  const network = hre.network.name;
+  console.log(
+    `Submit transactions with account: ${account.address} on ${network}`
+  );
 
-    const deployer = new Deployer(account_num, 3)
+  const deployer = new Deployer(account_num, 3);
 
-    const prompt = require('prompt-sync')();
-    const iscontinue = prompt("continue (y/n/_): ")
-    if (iscontinue !== "y") {
-        console.log("end")
-        return
-    }
-
+  const prompt = require("prompt-sync")();
+  const iscontinue = prompt("continue (y/n/_): ");
+  if (iscontinue !== "y") {
+    console.log("end");
+    return;
+  }
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
